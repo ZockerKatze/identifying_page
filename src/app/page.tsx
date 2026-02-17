3c9c046641fc322c27b76f7b2e880eb14afc1b0b
rattatwinko <seppmutterman@gmail.com>
2026-02-17 19:34:53 +0100

    changes for java applet clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,17 @@ import TechStackSection from '../components/TechStackSection'
 import InfrastructureSection from '../components/InfrastructureSection'
 import ContactSection from '../components/ContactSection'
 import Footer from '../components/Footer'
+import JavaApplet from '@/components/JavaApplets'
 
 export default function Home() {
   return (
     <div className="container">
+      <JavaApplet />
       <Header />
       <AboutSection />
       <ExperienceSection />
       <TechStackSection />
       <InfrastructureSection />
-      { /* <ProjectsSection />  ; dont work */ }
       <ContactSection />
       <Footer />
     </div>
---
+++ src/components/JavaApplets.tsx
@@ -0,0 +1,35 @@
+// components/JavaApplet.tsx
+import Link from 'next/link';
+import styles from './JavaApplet.module.css';
+
+export default function JavaApplet() {
+  return (
+    <div className={styles.container}>
+      <Link href="java" className={styles.link}>
+        
+        <svg 
+          width="70" 
+          height="60" 
+          viewBox="0 0 100 100" 
+          className={styles.arrowSvg}
+        >
+          <path 
+            d="M 10 60 C 0 80, 20 90, 30 70 C 40 50, 60 30, 90 20 M 75 15 L 90 20 L 80 35" 
+            fill="none" 
+            strokeWidth="4" 
+            strokeLinecap="round"
+            className={styles.arrowPath}
+          />
+        </svg>
+
+        {/* svg java logo */}
+        <img
+          src="/demoimages/java.svg"
+          alt="Java Logo"
+          className={styles.javaLogo}
+        />
+        
+      </Link>
+    </div>
+  );
+};
